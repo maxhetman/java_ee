21606929dc96d1bb63efe379c7bfa13bfa3b42ba
Max Hetman <maxxhetman@gmail.com>
2018-02-18 21:00:36 +0200

    aspect j xml

---
+++ AspectJ/src/main/java/com/hetman/Listener.java
@@ -0,0 +1,20 @@
+package com.hetman;
+
+public class Listener {
+
+    public void startListen()
+    {
+        System.out.println("I'd like to listen for an interesting speech...");
+    }
+
+    public void endListenFine(String topic)
+    {
+        System.out.println("It was an interesting speech about " + topic);
+    }
+
+    public void endListenError()
+    {
+        System.out.println("It is something wrong with your speech...");
+    }
+
+}
