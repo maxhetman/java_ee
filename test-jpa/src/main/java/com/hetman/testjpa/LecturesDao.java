907532fd7fbeaa52bd9a37b48e907153506b8d8d
Max Hetman <maxxhetman@gmail.com>
2018-03-17 18:55:53 +0200

    jpa

---
+++ test-jpa/src/main/java/com/hetman/testjpa/LecturesDao.java
@@ -0,0 +1,10 @@
+package com.hetman.testjpa;
+
+public interface LecturesDao {
+    Lecture addLecture(Lecture lecture);
+
+    Lecture getLecture(int id);
+
+    void saveLecture(Lecture lecture);
+
+}
